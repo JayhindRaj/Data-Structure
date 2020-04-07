a85b768533764b8799d355d7cdece0c713727f4c
Jayhind Rajpoot <jayhindraj@outlook.com>
2020-04-07 19:02:47 +0530

    initial commit

---
+++ src/main/java/com/jay/ds/array/ArrayEx.java
@@ -0,0 +1,14 @@
+package com.jay.ds.array;
+
+public class ArrayEx {
+
+	
+	public static void main(String[] args) {
+		test1();
+	}
+	
+	public static void test1() {
+		int[] arr = new int[] {};
+		System.out.println("Length: " + arr.length);
+	}
+}
